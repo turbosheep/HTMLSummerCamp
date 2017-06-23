ad1e50ba4daed3d4a2222f471883c25072769155
Hannah Eyre <turbosheep@users.noreply.github.com>
2017-06-23 15:23:26 -0600

    aesthetic

--- gradient.js
+++ gradient.js
@@ -41,7 +41,8 @@ var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
 var color2 = "rgb("+r2+","+g2+","+b2+")";
 
  $('.gradient').css({
-   background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
+   background: "-webkit-radial-gradient(circle, "+color1+","+color2+")"}).css({
+ //  background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
     background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
   
   step += gradientSpeed;
